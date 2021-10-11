ba2f552f4b93330a191da0a8dc559c522d2e013d
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-11 14:17:19 +0300

    add logic to custom quote model class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,9 @@
 export class Quote {
+    public showAuthor: boolean;
+    likes!: number;
+    dislikes!: number;
+    showYourName: any;
+    constructor(public id: number, public yourName: string, public name: string,public author: string,  public completeDate: Date){
+      this.showAuthor=false;
+    }
 }
