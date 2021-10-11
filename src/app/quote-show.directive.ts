821709425d7fd3a253077ea030bd8d0dd528dde3
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-11 14:11:02 +0300

    add logic to show-directive

--- src/app/quote-show.directive.ts
+++ src/app/quote-show.directive.ts
@@ -5,6 +5,17 @@ import { Directive } from '@angular/core';
 })
 export class QuoteShowDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
 
 }
