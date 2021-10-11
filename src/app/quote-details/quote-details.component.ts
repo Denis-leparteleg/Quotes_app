e36789f32e38c0584629c4c63f95d578b8ef5477
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-11 10:49:57 +0300

    modify quote-details component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input()
+  quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  upvote: number = 0;
+  downvote: number = 0;
+  upvoteButtonClick() {
+    this.upvote++;
+  }
+  downvoteButtonClick(){
+    this.downvote++;
+  }
+    completeQuote(complete:boolean){
+      this.isComplete.emit(complete);
+    }
+  
+    deleteQuote(complete:boolean){
+      this.isComplete.emit(complete);
+    }
+  
+   
+    constructor() { }
+  
+    ngOnInit() {
+    }
 
   constructor() { }
 
